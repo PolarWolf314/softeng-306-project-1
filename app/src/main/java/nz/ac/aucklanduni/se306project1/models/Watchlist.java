799bd2edcc116a9ab5a2662fbf8cf63485aa5342
pumbas600 <joshjeffers600@gmail.com>
2023-08-09 23:18:42 +1200

    feat: :sparkles: Add watchlist model

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/models/Watchlist.java
@@ -0,0 +1,33 @@
+package nz.ac.aucklanduni.se306project1.models;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Watchlist {
+    private String userId;
+    private List<String> itemIds;
+
+    public Watchlist() {
+    }
+
+    public Watchlist(final String userId, final List<String> itemIds) {
+        this.userId = userId;
+        this.itemIds = itemIds;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public List<String> getItemIds() {
+        return Collections.unmodifiableList(this.itemIds);
+    }
+
+    public void addItemId(final String itemId) {
+        this.itemIds.add(itemId);
+    }
+
+    public void removeItemId(final String itemId) {
+        this.itemIds.remove(itemId);
+    }
+}
