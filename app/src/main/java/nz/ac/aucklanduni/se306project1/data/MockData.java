6070e68b3f3c62139d3ed36ab959b11f95b23d36
pumbas600 <joshjeffers600@gmail.com>
2023-08-15 12:51:30 +1200

    chore: :bricks: Add more mock data

--- app/src/main/java/nz/ac/aucklanduni/se306project1/data/MockData.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/data/MockData.java
@@ -36,6 +36,30 @@ public class MockData {
                             List.of("https://cdn.discordapp.com/attachments/713550649937231925/1140787296124289065/Hi-Vis.png"),
                             Map.of("s", 10, "m", 12, "l", 13))),
                     CivilSubcategory.PPE
+            ),
+            new CivilItem(
+                    "d2d6a453-8870-48ee-98de-49a596c9de7c",
+                    "The Hard Helmet",
+                    "No Civil Engineer can be caught without their hard helmet...",
+                    CIVIL_CATEGORY,
+                    39D,
+                    List.of(new ColouredItemInformation(
+                            "#F26F18",
+                            List.of("https://cdn.discordapp.com/attachments/713550649937231925/1140786088554799174/Hard_Hat.png"),
+                            Map.of("s", 10, "m", 12, "l", 13))),
+                    CivilSubcategory.PPE
+            ),
+            new CivilItem(
+                    "e50fb9a4-4f49-4517-b60c-f401e57fd40d",
+                    "Hi-Vis Safety Vest",
+                    "We love safety vests :)",
+                    CIVIL_CATEGORY,
+                    11.99D,
+                    List.of(new ColouredItemInformation(
+                            "#C0F147",
+                            List.of("https://cdn.discordapp.com/attachments/713550649937231925/1140787296124289065/Hi-Vis.png"),
+                            Map.of("s", 10, "m", 12, "l", 13))),
+                    CivilSubcategory.PPE
             )
     ));
 }
