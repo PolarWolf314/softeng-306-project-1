42a49ff1d36d4eab5df132e862a9ca865919f51d
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-15 22:55:47 +1200

    feat: ✨ featured products now in activity home

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/HomeActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/HomeActivity.java
@@ -3,31 +3,31 @@ package nz.ac.aucklanduni.se306project1.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import nz.ac.aucklanduni.se306project1.adapters.ListRecyclerAdapter;
 import nz.ac.aucklanduni.se306project1.data.MockData;
-import nz.ac.aucklanduni.se306project1.databinding.ActivityCategoryBinding;
-import nz.ac.aucklanduni.se306project1.itemdecorations.GridSpacingItemDecoration;
+import nz.ac.aucklanduni.se306project1.databinding.ActivityHomeBinding;
 import nz.ac.aucklanduni.se306project1.models.items.Item;
 import nz.ac.aucklanduni.se306project1.viewholders.FeaturedItemCardViewHolder;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ActivityCategoryBinding binding;
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        this.binding = ActivityCategoryBinding.inflate(this.getLayoutInflater());
+        this.binding = ActivityHomeBinding.inflate(this.getLayoutInflater());
         this.setContentView(this.binding.getRoot());
 
-        final RecyclerView recyclerView = this.binding.categoryListRecyclerView;
+        final RecyclerView recyclerView = this.binding.featuredProductsRecyclerView;
         final ListRecyclerAdapter<Item, ?> adapter = new ListRecyclerAdapter<>(
                 this, MockData.ITEMS, FeaturedItemCardViewHolder.Builder.INSTANCE);
 
         recyclerView.setAdapter(adapter);
-        GridSpacingItemDecoration.attachGrid(recyclerView, this, 1, 12, 20);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
     }
 }
