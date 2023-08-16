4b485918eb4b0207864cadbb8cde1519f35acdbf
pumbas600 <joshjeffers600@gmail.com>
2023-08-16 20:40:39 +1200

    feat :sparkles: Create a list diff callback class

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/utils/ListDiff.java
@@ -0,0 +1,36 @@
+package nz.ac.aucklanduni.se306project1.utils;
+
+import androidx.recyclerview.widget.DiffUtil;
+
+import java.util.List;
+
+public class ListDiff<Item> extends DiffUtil.Callback {
+
+    private final List<Item> oldList;
+    private final List<Item> newList;
+
+    public ListDiff(final List<Item> oldList, final List<Item> newList) {
+        this.oldList = oldList;
+        this.newList = newList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return this.oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return this.newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(final int oldItemPosition, final int newItemPosition) {
+        return this.oldList.get(oldItemPosition).equals(this.newList.get(newItemPosition));
+    }
+
+    @Override
+    public boolean areContentsTheSame(final int oldItemPosition, final int newItemPosition) {
+        return this.oldList.get(oldItemPosition).equals(this.newList.get(newItemPosition));
+    }
+}
