679031391bd519b90d9144da8893eb67e1f996e8
pumbas600 <joshjeffers600@gmail.com>
2023-08-16 19:27:44 +1200

    feat: :sparkles: Create concrete cart item search view modal

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/CartItemSearchViewModel.java
@@ -0,0 +1,9 @@
+package nz.ac.aucklanduni.se306project1.viewmodels;
+
+import nz.ac.aucklanduni.se306project1.models.items.CartItem;
+
+/**
+ * A concrete class that enables filtering for {@link CartItem cart items}.
+ */
+public class CartItemSearchViewModel extends SearchViewModel<CartItem> {
+}
--- app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/ItemSearchViewModel.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/ItemSearchViewModel.java
@@ -3,7 +3,7 @@ package nz.ac.aucklanduni.se306project1.viewmodels;
 import nz.ac.aucklanduni.se306project1.models.items.Item;
 
 /**
- * A concrete class that defines the filtering system for {@link Item items}.
+ * A concrete class that enables filtering for {@link Item items}.
  */
 public class ItemSearchViewModel extends SearchViewModel<Item> {
 }
