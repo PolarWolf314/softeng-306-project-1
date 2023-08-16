7e4fe91021277bd6dc67456b333d07f2ef207b80
pumbas600 <joshjeffers600@gmail.com>
2023-08-16 17:58:43 +1200

    feat: :sparkles: Create concrete item search view model

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/ItemSearchViewModel.java
@@ -0,0 +1,9 @@
+package nz.ac.aucklanduni.se306project1.viewmodels;
+
+import nz.ac.aucklanduni.se306project1.models.items.Item;
+
+/**
+ * A concrete class that defines the filtering system for {@link Item items}.
+ */
+public class ItemSearchViewModel extends SearchViewModel<Item> {
+}
