9cb389830f454f46b83d1b1872138c8aa46130db
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-16 20:04:55 +1200

    feat: ✨ added logic to handle collapsing the view

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/ListActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/ListActivity.java
@@ -1,16 +1,28 @@
 package nz.ac.aucklanduni.se306project1.activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.appbar.AppBarLayout;
 
 import nz.ac.aucklanduni.se306project1.R;
 
 public class ListActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        this.setContentView(R.layout.activity_list);
+
+        final AppBarLayout appBarLayout = this.findViewById(R.id.app_bar_layout);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(final AppBarLayout appBarLayout, final int verticalOffset) {
+                if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange() - 124) {
+                    appBarLayout.setExpanded(false, true);
+                }
+            }
+        });
     }
 }
