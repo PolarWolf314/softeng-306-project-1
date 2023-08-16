43082c657ca001fd7b310f6057562bb39fca853b
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-16 14:48:14 +1200

    fix: 🐛 fixed breaking changes from rebasing main

--- app/src/main/java/nz/ac/aucklanduni/se306project1/viewholders/FeaturedItemCardViewHolder.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewholders/FeaturedItemCardViewHolder.java
@@ -1,5 +1,6 @@
 package nz.ac.aucklanduni.se306project1.viewholders;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.view.View;
 import android.widget.CheckBox;
@@ -9,22 +10,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.Locale;
 
 import nz.ac.aucklanduni.se306project1.R;
+import nz.ac.aucklanduni.se306project1.models.ImageInfo;
+import nz.ac.aucklanduni.se306project1.models.items.ColouredItemInformation;
 import nz.ac.aucklanduni.se306project1.models.items.Item;
 
 public class FeaturedItemCardViewHolder extends BindableViewHolder<Item> {
 
+    private final Context context;
     private final CardView cardView;
     private final ImageView itemImage;
     private final TextView itemName;
     private final TextView itemPrice;
     private final CheckBox favouriteItemCheckbox;
 
-    public FeaturedItemCardViewHolder(@NonNull final View itemView) {
+    public FeaturedItemCardViewHolder(@NonNull final Context context, @NonNull final View itemView) {
         super(itemView);
 
+        this.context = context;
         this.cardView = itemView.findViewById(R.id.item_card);
         this.itemImage = itemView.findViewById(R.id.item_card_image);
         this.itemName = itemView.findViewById(R.id.item_card_name);
@@ -39,9 +46,12 @@ public class FeaturedItemCardViewHolder extends BindableViewHolder<Item> {
                     String.format("The item %s (%s) has no colour information", item.getDisplayName(), item.getId()));
         }
 
-        this.cardView.setCardBackgroundColor(Color.parseColor(item.getColours().get(0).getColour()));
-        // TODO: Convert image URL to image
-        // this.itemImage = ...
+        final ColouredItemInformation colourInformation = item.getColours().get(0);
+        final ImageInfo imageInfo = colourInformation.getImages().get(0);
+
+        Glide.with(this.context).load(imageInfo.getUrl()).into(this.itemImage);
+        this.itemImage.setContentDescription(imageInfo.getDescription());
+        this.cardView.setCardBackgroundColor(Color.parseColor(colourInformation.getColour()));
         this.itemName.setText(item.getDisplayName());
         this.itemPrice.setText(String.format(Locale.getDefault(), "$%.2f", item.getPrice()));
         this.favouriteItemCheckbox.setOnCheckedChangeListener((button, isChecked) -> {
@@ -51,7 +61,7 @@ public class FeaturedItemCardViewHolder extends BindableViewHolder<Item> {
 
     public static class Builder implements ViewHolderBuilder<FeaturedItemCardViewHolder> {
 
-        public static final FeaturedItemCardViewHolder.Builder INSTANCE = new FeaturedItemCardViewHolder.Builder();
+        public static final Builder INSTANCE = new Builder();
 
         @Override
         public int getLayoutId() {
@@ -59,8 +69,8 @@ public class FeaturedItemCardViewHolder extends BindableViewHolder<Item> {
         }
 
         @Override
-        public FeaturedItemCardViewHolder createViewHolder(final View view) {
-            return new FeaturedItemCardViewHolder(view);
+        public FeaturedItemCardViewHolder createViewHolder(final Context context, final View view) {
+            return new FeaturedItemCardViewHolder(context, view);
         }
     }
 }
