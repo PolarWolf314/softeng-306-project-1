3f675994fb37af082173694b07c1c72f17d7a7d1
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-16 20:30:14 +1200

    feat: ✨ added in placeholder for recycler view
    - also removed sticky header logic since it wasn't working. Will do later if there is more time.

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/ListActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/ListActivity.java
@@ -2,27 +2,35 @@ package nz.ac.aucklanduni.se306project1.activities;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.appbar.AppBarLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import nz.ac.aucklanduni.se306project1.R;
+import nz.ac.aucklanduni.se306project1.adapters.ListRecyclerAdapter;
+import nz.ac.aucklanduni.se306project1.data.MockData;
+import nz.ac.aucklanduni.se306project1.databinding.ActivityListBinding;
+import nz.ac.aucklanduni.se306project1.itemdecorations.GridSpacingItemDecoration;
+import nz.ac.aucklanduni.se306project1.models.items.Item;
+import nz.ac.aucklanduni.se306project1.viewholders.ItemCardViewHolder;
 
 public class ListActivity extends AppCompatActivity {
+
+    private @NonNull ActivityListBinding binding;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_list);
 
-        final AppBarLayout appBarLayout = this.findViewById(R.id.app_bar_layout);
-        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(final AppBarLayout appBarLayout, final int verticalOffset) {
-                if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange() - 124) {
-                    appBarLayout.setExpanded(false, true);
-                }
-            }
-        });
+        this.binding = ActivityListBinding.inflate(this.getLayoutInflater());
+        this.setContentView(this.binding.getRoot());
+
+        final RecyclerView recyclerView = this.binding.listRecyclerView;
+        final ListRecyclerAdapter<Item, ?> adapter = new ListRecyclerAdapter<>(
+                this, MockData.ITEMS, ItemCardViewHolder.Builder.INSTANCE);
+
+        recyclerView.setAdapter(adapter);
+        GridSpacingItemDecoration.attachGrid(recyclerView, this, 2, 12, 20);
     }
 }
