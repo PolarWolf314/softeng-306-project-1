dce227d0b5f3a41360f3d18a8466fd0a93787a1f
pumbas600 <joshjeffers600@gmail.com>
2023-08-17 08:41:59 +1200

    chore: :bricks: Remove unused getters in ListDiff

--- app/src/main/java/nz/ac/aucklanduni/se306project1/utils/ListDiff.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/utils/ListDiff.java
@@ -3,7 +3,6 @@ package nz.ac.aucklanduni.se306project1.utils;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
-import java.util.Collections;
 import java.util.List;
 
 public class ListDiff<Item> extends DiffUtil.Callback {
@@ -41,13 +40,5 @@ public class ListDiff<Item> extends DiffUtil.Callback {
     public Object getChangePayload(final int oldItemPosition, final int newItemPosition) {
         // TODO: Implement this when we get to adding animations (ItemAnimator)
         return super.getChangePayload(oldItemPosition, newItemPosition);
-    }
-
-    public List<Item> getOldList() {
-        return Collections.unmodifiableList(this.oldList);
-    }
-
-    public List<Item> getNewList() {
-        return Collections.unmodifiableList(this.newList);
     }
 }
