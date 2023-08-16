eca4ef29933fe4d5e46845643a53f50764dc0d61
pumbas600 <joshjeffers600@gmail.com>
2023-08-16 17:57:03 +1200

    docs: :memo: Add javadocs to search view model class

--- app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/SearchViewModel.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/SearchViewModel.java
@@ -20,27 +20,53 @@ public abstract class SearchViewModel<Item> extends ViewModel {
         this.filters.observeForever((filters) -> this.applyFilters());
     }
 
+    /**
+     * Removes this filter from the list of filters being applied to the items. This will cause the
+     * filtered items to be recalculated.
+     *
+     * @param filter The filter to remove
+     */
     public void removeFilter(final Predicate<Item> filter) {
         final List<Predicate<Item>> currentFilters = this.getFilters();
         currentFilters.remove(filter);
         this.filters.setValue(currentFilters);
     }
 
+    /**
+     * Adds this filter to the list of filters being applied to the items. This will cause the
+     * filtered items to be recalculated.
+     *
+     * @param filter The filter to add
+     */
     public void addFilter(final Predicate<Item> filter) {
         final List<Predicate<Item>> currentFilters = this.getFilters();
         currentFilters.add(filter);
         this.filters.setValue(currentFilters);
     }
 
+    /**
+     * Update the list of unfiltered items. This will cause the filtered items to be recalculated
+     * using the current filters.
+     *
+     * @param items The {@link List} of unfiltered items to use
+     */
     public void setOriginalItems(final List<Item> items) {
         this.originalItems = items;
         this.applyFilters();
     }
 
+    /**
+     * Retrieve an observable list of the current filtered items.
+     *
+     * @return The {@link LiveData} wrapped list of items.
+     */
     public LiveData<List<Item>> getFilteredItems() {
         return this.filteredItems;
     }
 
+    /**
+     * Updates the filtered items based on the current filters set.
+     */
     private void applyFilters() {
         final List<Item> filteredItems = this.originalItems.stream()
                 .filter(this::matchesFilters)
@@ -49,10 +75,22 @@ public abstract class SearchViewModel<Item> extends ViewModel {
         this.filteredItems.setValue(filteredItems);
     }
 
+    /**
+     * Determines if the specified {@link Item} matches all the current filters set.
+     *
+     * @param item The item to check the filters against
+     * @return Whether the item matches all the current filters
+     */
     private boolean matchesFilters(final Item item) {
         return this.getFilters().stream().allMatch(filter -> filter.test(item));
     }
 
+    /**
+     * Retrieves the current list of filters. If there is none, a new empty {@link ArrayList} is
+     * returned.
+     *
+     * @return The {@link List} of current filters
+     */
     private List<Predicate<Item>> getFilters() {
         final List<Predicate<Item>> filters = this.filters.getValue();
         // This is required as the returned value is marked as nullable
