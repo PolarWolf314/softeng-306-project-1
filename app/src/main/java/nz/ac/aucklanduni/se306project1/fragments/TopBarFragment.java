12fa2f71503980b0520daa18783b752544e0107f
pumbas600 <joshjeffers600@gmail.com>
2023-08-17 21:27:30 +1200

    feat: :sparkles: Add simple toggle search bar logic

--- app/src/main/java/nz/ac/aucklanduni/se306project1/fragments/TopBarFragment.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/fragments/TopBarFragment.java
@@ -5,16 +5,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import nz.ac.aucklanduni.se306project1.R;
+import nz.ac.aucklanduni.se306project1.databinding.FragmentTopBarBinding;
 
 public class TopBarFragment extends Fragment {
 
+    private FragmentTopBarBinding binding;
+
     @Override
-    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
-                             final Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_bar, container, false);
+    public View onCreateView(
+            @NonNull final LayoutInflater inflater,
+            final ViewGroup container,
+            final Bundle savedInstanceState
+    ) {
+        this.binding = FragmentTopBarBinding.inflate(inflater, container, false);
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.binding.endIconButton.setOnClickListener(v -> this.showSearchBar());
+    }
+
+    private void showSearchBar() {
+        this.binding.startIconButton.setVisibility(View.GONE);
+        this.binding.topBarTitle.setVisibility(View.GONE);
+        this.binding.endIconButton.setVisibility(View.GONE);
+        this.binding.searchBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideSearchBar() {
+        this.binding.startIconButton.setVisibility(View.VISIBLE);
+        this.binding.topBarTitle.setVisibility(View.VISIBLE);
+        this.binding.endIconButton.setVisibility(View.VISIBLE);
+        this.binding.searchBar.setVisibility(View.GONE);
     }
 }
