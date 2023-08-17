0cc1e5fa718a2c27ba37b8ea21428d02436aa526
pumbas600 <joshjeffers600@gmail.com>
2023-08-17 21:15:29 +1200

    chore: :bricks: Remove auto generated code in Fragment

--- app/src/main/java/nz/ac/aucklanduni/se306project1/fragments/TopBarFragment.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/fragments/TopBarFragment.java
@@ -9,52 +9,7 @@ import androidx.fragment.app.Fragment;
 
 import nz.ac.aucklanduni.se306project1.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TopBarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TopBarFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TopBarFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TopBarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TopBarFragment newInstance(final String param1, final String param2) {
-        final TopBarFragment fragment = new TopBarFragment();
-        final Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (this.getArguments() != null) {
-            this.mParam1 = this.getArguments().getString(ARG_PARAM1);
-            this.mParam2 = this.getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
