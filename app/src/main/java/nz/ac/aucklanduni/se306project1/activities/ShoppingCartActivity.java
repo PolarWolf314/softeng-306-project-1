9a945631b17b51430081f0799810e801f6346d29
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-18 20:21:58 +1200

    feat: ✨ made files for activity and item card

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/ShoppingCartActivity.java
@@ -0,0 +1,16 @@
+package nz.ac.aucklanduni.se306project1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nz.ac.aucklanduni.se306project1.R;
+
+public class ShoppingCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+}
