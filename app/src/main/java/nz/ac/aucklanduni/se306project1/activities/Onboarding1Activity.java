8570fa61e4837541093b2b45d765faf4a59131a0
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-22 00:18:30 +1200

    feat: ✨ switching to second onboarding page

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/Onboarding1Activity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/Onboarding1Activity.java
@@ -1,16 +1,26 @@
 package nz.ac.aucklanduni.se306project1.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
 
 import nz.ac.aucklanduni.se306project1.R;
 
 public class Onboarding1Activity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding1);
+        this.setContentView(R.layout.activity_onboarding1);
+
+
+        final MaterialButton nextButton = this.findViewById(R.id.next_button_onboarding1);
+        nextButton.setOnClickListener(v -> {
+            final Intent intent = new Intent(Onboarding1Activity.this, Onboarding2Activity.class);
+            Onboarding1Activity.this.startActivity(intent);
+        });
     }
 }
