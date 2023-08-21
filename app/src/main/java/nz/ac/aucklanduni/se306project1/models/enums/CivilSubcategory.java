47fee8b6282be65171996422ca3aee5bf54c4d26
pumbas600 <joshjeffers600@gmail.com>
2023-08-22 00:13:32 +1200

    feat: :sparkles: Create subcategory interface implemented by the civil and software subcategories

--- app/src/main/java/nz/ac/aucklanduni/se306project1/models/enums/CivilSubcategory.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/models/enums/CivilSubcategory.java
@@ -1,5 +1,24 @@
 package nz.ac.aucklanduni.se306project1.models.enums;
 
-public enum CivilSubcategory {
-    PPE, CLOTHING, VEHICLES, TOOLS
+import androidx.annotation.StringRes;
+
+import nz.ac.aucklanduni.se306project1.R;
+import nz.ac.aucklanduni.se306project1.models.Subcategory;
+
+public enum CivilSubcategory implements Subcategory {
+    PPE(R.string.civil_subcategory_ppe),
+    CLOTHING(R.string.civil_subcategory_clothing),
+    VEHICLES(R.string.civil_subcategory_vehicles),
+    TOOLS(R.string.civil_subcategory_tools);
+
+    private final int displayNameId;
+
+    CivilSubcategory(@StringRes final int displayNameId) {
+        this.displayNameId = displayNameId;
+    }
+
+    @Override
+    public int getDisplayNameId() {
+        return this.displayNameId;
+    }
 }
