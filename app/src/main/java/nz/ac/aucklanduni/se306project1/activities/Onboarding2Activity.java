332440e0b8b88657363a1d08a378bb19b3e3fc85
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-22 00:06:28 +1200

    feat: ✨ completed second onboarding page

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/Onboarding2Activity.java
@@ -0,0 +1,16 @@
+package nz.ac.aucklanduni.se306project1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nz.ac.aucklanduni.se306project1.R;
+
+public class Onboarding2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding2);
+    }
+}
