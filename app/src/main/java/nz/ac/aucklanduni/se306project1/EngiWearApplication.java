3f2f9aba20ee2576606269a42df4d41737a3509c
pumbas600 <joshjeffers600@gmail.com>
2023-08-21 14:52:50 +1200

    feat: :sparkles: Create application class

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/EngiWearApplication.java
@@ -0,0 +1,11 @@
+package nz.ac.aucklanduni.se306project1;
+
+import android.app.Application;
+
+public class EngiWearApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
