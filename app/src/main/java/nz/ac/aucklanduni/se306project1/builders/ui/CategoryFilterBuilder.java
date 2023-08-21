1a90d644076521b5491c85cefc00dfed68ee665b
pumbas600 <joshjeffers600@gmail.com>
2023-08-21 23:54:18 +1200

    feat: :sparkles: Create CategoryFilterBuilder interface

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/builders/ui/CategoryFilterBuilder.java
@@ -0,0 +1,21 @@
+package nz.ac.aucklanduni.se306project1.builders.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+
+import nz.ac.aucklanduni.se306project1.viewmodels.ItemSearchViewModel;
+
+public interface CategoryFilterBuilder {
+
+    /**
+     * Constructs the view which will manage the category-specific filtering. The created UI will
+     * be injected into the top bar. Filtering can be achieved by adding filters to the provided
+     * {@link ItemSearchViewModel}.
+     *
+     * @param inflater        The {@link LayoutInflater}
+     * @param searchViewModel The {@link ItemSearchViewModel} to use for applying filters to the
+     *                        current items
+     * @return The category-specific ui to inject into the top bar
+     */
+    View buildFilteringView(LayoutInflater inflater, ItemSearchViewModel searchViewModel);
+}
