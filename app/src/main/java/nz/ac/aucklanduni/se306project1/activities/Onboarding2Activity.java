23021e7cb97d88b1cfb254da8665404218b70e3f
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-22 00:21:19 +1200

    feat: ✨ transition to third screen

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/Onboarding2Activity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/Onboarding2Activity.java
@@ -1,16 +1,27 @@
 package nz.ac.aucklanduni.se306project1.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.button.MaterialButton;
 
 import nz.ac.aucklanduni.se306project1.R;
 
 public class Onboarding2Activity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding2);
+        this.setContentView(R.layout.activity_onboarding2);
+
+
+        final MaterialButton nextButton = this.findViewById(R.id.next_button_onboarding2);
+        nextButton.setOnClickListener(v -> {
+            final Intent intent = new Intent(Onboarding2Activity.this, Onboarding3Activity.class);
+            Onboarding2Activity.this.startActivity(intent);
+            this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        });
     }
 }
