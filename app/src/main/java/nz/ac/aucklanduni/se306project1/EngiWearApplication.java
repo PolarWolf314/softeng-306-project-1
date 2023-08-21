2a78c9013fbd14d3a08af54dacc4a3576811221b
pumbas600 <joshjeffers600@gmail.com>
2023-08-21 14:56:55 +1200

    feat: :sparkles: Add all the providers and their respective getters and setters

--- app/src/main/java/nz/ac/aucklanduni/se306project1/EngiWearApplication.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/EngiWearApplication.java
@@ -2,10 +2,50 @@ package nz.ac.aucklanduni.se306project1;
 
 import android.app.Application;
 
+import androidx.annotation.Nullable;
+
+import nz.ac.aucklanduni.se306project1.dataproviders.AuthenticationProvider;
+import nz.ac.aucklanduni.se306project1.dataproviders.FirebaseAuthenticationProvider;
+import nz.ac.aucklanduni.se306project1.dataproviders.FirebaseItemDataProvider;
+import nz.ac.aucklanduni.se306project1.dataproviders.ItemDataProvider;
+import nz.ac.aucklanduni.se306project1.dataproviders.UserDataProvider;
+
 public class EngiWearApplication extends Application {
+
+    @Nullable
+    private UserDataProvider userDataProvider;
+    private AuthenticationProvider authenticationProvider;
+    private ItemDataProvider itemDataProvider;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        this.itemDataProvider = new FirebaseItemDataProvider();
+        this.authenticationProvider = new FirebaseAuthenticationProvider();
+    }
+
+    @Nullable
+    public UserDataProvider getUserDataProvider() {
+        return this.userDataProvider;
+    }
+
+    public void setUserDataProvider(@Nullable final UserDataProvider userDataProvider) {
+        this.userDataProvider = userDataProvider;
+    }
+
+    public AuthenticationProvider getAuthenticationProvider() {
+        return this.authenticationProvider;
+    }
+
+    public void setAuthenticationProvider(final AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    public ItemDataProvider getItemDataProvider() {
+        return this.itemDataProvider;
+    }
+
+    public void setItemDataProvider(final ItemDataProvider itemDataProvider) {
+        this.itemDataProvider = itemDataProvider;
     }
 }
