0d8328b69d651674a2506ab33e4fa47d0a8c011b
pumbas600 <joshjeffers600@gmail.com>
2023-08-22 15:34:44 +1200

    feat: :sparkles: Create detailed item view model

--- app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/DetailedItemViewModel.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/DetailedItemViewModel.java
@@ -7,17 +7,18 @@ import androidx.lifecycle.viewmodel.ViewModelInitializer;
 import nz.ac.aucklanduni.se306project1.EngiWearApplication;
 import nz.ac.aucklanduni.se306project1.dataproviders.ItemDataProvider;
 
-public class ListViewModel extends ViewModel {
-    public static final ViewModelInitializer<ListViewModel> INITIALIZER = new ViewModelInitializer<>(
-            ListViewModel.class,
+public class DetailedItemViewModel extends ViewModel {
+
+    public static final ViewModelInitializer<DetailedItemViewModel> INITIALIZER = new ViewModelInitializer<>(
+            DetailedItemViewModel.class,
             creationExtras -> {
                 final EngiWearApplication app = (EngiWearApplication) creationExtras.get(ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY);
                 assert app != null;
-                return new ListViewModel(app.getItemDataProvider());
+                return new DetailedItemViewModel(app.getItemDataProvider());
             });
     private final ItemDataProvider itemDataProvider;
 
-    public ListViewModel(final ItemDataProvider itemDataProvider) {
+    public DetailedItemViewModel(final ItemDataProvider itemDataProvider) {
         this.itemDataProvider = itemDataProvider;
     }
 
