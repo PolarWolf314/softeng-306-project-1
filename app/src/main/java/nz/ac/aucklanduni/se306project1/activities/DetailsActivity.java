354c7701045b059be9090ef274d4050436fcbded
pumbas600 <joshjeffers600@gmail.com>
2023-08-22 16:49:06 +1200

    feat: :sparkles: Dynamically generate colour selector

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/DetailsActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/DetailsActivity.java
@@ -1,10 +1,20 @@
 package nz.ac.aucklanduni.se306project1.activities;
 
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.radiobutton.MaterialRadioButton;
+
+import java.util.List;
+
+import nz.ac.aucklanduni.se306project1.R;
 import nz.ac.aucklanduni.se306project1.databinding.ActivityDetailsBinding;
+import nz.ac.aucklanduni.se306project1.models.items.ColouredItemInformation;
 import nz.ac.aucklanduni.se306project1.models.items.Item;
 import nz.ac.aucklanduni.se306project1.viewmodels.DetailsViewModel;
 
@@ -32,5 +42,33 @@ public class DetailsActivity extends TopBarActivity {
     private void bindItemData(final Item item) {
         this.topBarViewModel.setTitle(item.getDisplayName());
         System.out.println("Loaded item " + item.getDisplayName());
+
+        this.generateColourOptions(item);
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void generateColourOptions(final Item item) {
+        final RadioGroup radioGroup = this.binding.detailsItemColorSelectorRadioGroup;
+
+        final List<ColouredItemInformation> colouredItemInfo = item.getColours();
+
+        for (int index = 0; index < colouredItemInfo.size(); index++) {
+            final ColouredItemInformation colouredInfo = colouredItemInfo.get(index);
+
+            final MaterialRadioButton radio = new MaterialRadioButton(this);
+            radio.setButtonDrawable(R.drawable.colour_option_selector);
+            radio.setSupportButtonTintList(ColorStateList.valueOf(Color.parseColor(colouredInfo.getColour())));
+            radioGroup.addView(radio);
+
+            if (index == 0) {
+                // Note: We can only check it AFTER adding it to the radio group
+                radio.setChecked(true);
+            }
+        }
+
+        radioGroup.setOnCheckedChangeListener((group, checkedItemPosition) -> {
+            final int checkedItemIndex = checkedItemPosition - 1;
+            System.out.println("Selected: " + checkedItemIndex);
+        });
     }
 }
