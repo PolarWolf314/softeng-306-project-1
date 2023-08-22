4e8b1556f74c3779aa4bd8274e115f17e424f3b9
KW781 <aarnobguha@gmail.com>
2023-08-22 18:08:33 +1200

    feat: :sparkles: added binding to LoginActivity

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/LoginActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/LoginActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import nz.ac.aucklanduni.se306project1.R;
+import nz.ac.aucklanduni.se306project1.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.binding = ActivityLoginBinding.inflate(this.getLayoutInflater());
         setContentView(R.layout.activity_login);
+
+
     }
 }
