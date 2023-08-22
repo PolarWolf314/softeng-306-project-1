d6c656998771f0ed8fc9bf71dd389bc524ff1ccb
KW781 <aarnobguha@gmail.com>
2023-08-22 22:03:47 +1200

    feat: :sparkles: initialised signup activity properly

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/SignupActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/SignupActivity.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import nz.ac.aucklanduni.se306project1.R;
+import nz.ac.aucklanduni.se306project1.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
+    private ActivitySignupBinding binding;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_signup);
+
+        this.binding = ActivitySignupBinding.inflate(this.getLayoutInflater());
+        this.setContentView(this.binding.getRoot());
+
+
     }
 }
