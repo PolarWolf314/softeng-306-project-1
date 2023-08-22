e431ee801cacad2478a31587698e00d3c3aedacf
KW781 <aarnobguha@gmail.com>
2023-08-22 19:01:18 +1200

    feat: :sparkles: added method to authenticate user in activity

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/LoginActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/LoginActivity.java
@@ -1,11 +1,19 @@
 package nz.ac.aucklanduni.se306project1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import nz.ac.aucklanduni.se306project1.R;
 import nz.ac.aucklanduni.se306project1.databinding.ActivityLoginBinding;
+import nz.ac.aucklanduni.se306project1.dataproviders.UserDataProvider;
 import nz.ac.aucklanduni.se306project1.viewmodels.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,6 +28,22 @@ public class LoginActivity extends AppCompatActivity {
         this.binding = ActivityLoginBinding.inflate(this.getLayoutInflater());
         setContentView(R.layout.activity_login);
 
+        this.loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
 
+        final MaterialButton loginButton = this.findViewById(R.id.login_button);
+        loginButton.setOnClickListener(v -> {
+            this.authenticateUser().thenAccept(userDataProvider -> {
+
+            }).exceptionally(exception -> {
+                // write code to handle login failure
+            });
+        });
+    }
+
+    public CompletableFuture<UserDataProvider> authenticateUser() {
+        String email = this.loginViewModel.getEmail();
+        String password = this.loginViewModel.getPassword();
+        return this.loginViewModel.getAuthenticationProvider().loginUser(email, password).thenApply(
+                userDataProvider -> userDataProvider).exceptionally(exception -> null);
     }
 }
--- app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/LoginViewModel.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/LoginViewModel.java
@@ -2,8 +2,11 @@ package nz.ac.aucklanduni.se306project1.viewmodels;
 
 import androidx.lifecycle.ViewModel;
 
+import java.util.concurrent.CompletableFuture;
+
 import nz.ac.aucklanduni.se306project1.dataproviders.AuthenticationProvider;
 import nz.ac.aucklanduni.se306project1.dataproviders.FirebaseAuthenticationProvider;
+import nz.ac.aucklanduni.se306project1.dataproviders.UserDataProvider;
 
 public class LoginViewModel extends ViewModel {
     private String email;
