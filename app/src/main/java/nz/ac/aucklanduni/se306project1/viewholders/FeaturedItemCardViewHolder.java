4158c543257ad5a4094bfe33c179cdc9ec5740d0
KW781 <aarnobguha@gmail.com>
2023-08-24 22:51:23 +1200

    feat: :sparkles: implemented adding of featured item to watchlist

--- app/src/main/java/nz/ac/aucklanduni/se306project1/viewholders/FeaturedItemCardViewHolder.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewholders/FeaturedItemCardViewHolder.java
@@ -14,6 +14,7 @@ import com.bumptech.glide.Glide;
 
 import java.util.Locale;
 
+import nz.ac.aucklanduni.se306project1.EngiWearApplication;
 import nz.ac.aucklanduni.se306project1.R;
 import nz.ac.aucklanduni.se306project1.models.ImageInfo;
 import nz.ac.aucklanduni.se306project1.models.items.ColouredItemInformation;
@@ -21,17 +22,17 @@ import nz.ac.aucklanduni.se306project1.models.items.Item;
 
 public class FeaturedItemCardViewHolder extends BindableViewHolder<Item> {
 
-    private final Context context;
+    private final EngiWearApplication engiWear;
     private final CardView cardView;
     private final ImageView itemImage;
     private final TextView itemName;
     private final TextView itemPrice;
     private final CheckBox favouriteItemCheckbox;
 
-    public FeaturedItemCardViewHolder(@NonNull final Context context, @NonNull final View itemView) {
+    public FeaturedItemCardViewHolder(@NonNull final EngiWearApplication engiWear, @NonNull final View itemView) {
         super(itemView);
 
-        this.context = context;
+        this.engiWear = engiWear;
         this.cardView = itemView.findViewById(R.id.item_card);
         this.itemImage = itemView.findViewById(R.id.item_card_image);
         this.itemName = itemView.findViewById(R.id.item_card_name);
@@ -49,13 +50,20 @@ public class FeaturedItemCardViewHolder extends BindableViewHolder<Item> {
         final ColouredItemInformation colourInformation = item.getColours().get(0);
         final ImageInfo imageInfo = colourInformation.getImages().get(0);
 
-        Glide.with(this.context).load(imageInfo.getUrl()).into(this.itemImage);
+        Glide.with(this.engiWear).load(imageInfo.getUrl()).into(this.itemImage);
         this.itemImage.setContentDescription(imageInfo.getDescription());
         this.cardView.setCardBackgroundColor(Color.parseColor(colourInformation.getColour()));
         this.itemName.setText(item.getDisplayName());
         this.itemPrice.setText(String.format(Locale.getDefault(), "$%.2f", item.getPrice()));
+        this.engiWear.getUserDataProvider().existsInWatchlist(item.getId()).thenAccept(inWatchlist -> {
+            this.favouriteItemCheckbox.setChecked(inWatchlist);
+        });
         this.favouriteItemCheckbox.setOnCheckedChangeListener((button, isChecked) -> {
-            // TODO: Toggle whether the item is in the watchlist
+            if (isChecked) {
+                this.engiWear.getUserDataProvider().addToWatchlist(item.getId());
+            } else {
+                this.engiWear.getUserDataProvider().removeFromWatchlist(item.getId());
+            }
         });
     }
 
@@ -70,7 +78,7 @@ public class FeaturedItemCardViewHolder extends BindableViewHolder<Item> {
 
         @Override
         public FeaturedItemCardViewHolder createViewHolder(final Context context, final View view) {
-            return new FeaturedItemCardViewHolder(context, view);
+            return new FeaturedItemCardViewHolder((EngiWearApplication) context, view);
         }
     }
 }
