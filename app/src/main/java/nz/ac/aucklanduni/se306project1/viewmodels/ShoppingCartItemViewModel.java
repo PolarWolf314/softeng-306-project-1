bc4adfde52d970887c90378eebb1039d02ef8835
KW781 <aarnobguha@gmail.com>
2023-08-26 04:58:09 +1200

    feat: :sparkles: added methods in view model to alter cart item quantity

--- app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/ShoppingCartItemViewModel.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/ShoppingCartItemViewModel.java
@@ -8,6 +8,8 @@ import java.util.Set;
 
 import nz.ac.aucklanduni.se306project1.dataproviders.UserDataProvider;
 import nz.ac.aucklanduni.se306project1.models.items.CartItem;
+import nz.ac.aucklanduni.se306project1.models.items.Item;
+import nz.ac.aucklanduni.se306project1.models.items.SerializedCartItem;
 
 public class ShoppingCartItemViewModel extends ItemSearchViewModel {
 
@@ -18,5 +20,41 @@ public class ShoppingCartItemViewModel extends ItemSearchViewModel {
     public ShoppingCartItemViewModel(final UserDataProvider userDataProvider) {
         this.userDataProvider = userDataProvider;
         this.userDataProvider.getShoppingCart().thenAccept(this.shoppingCartItems::setValue);
+    }
+
+    public void incrementItemQuantity(final CartItem cartItem) {
+        this.userDataProvider.incrementShoppingCartItemQuantity(new SerializedCartItem(cartItem.getQuantity(),
+                cartItem.getColour(), cartItem.getSize(), cartItem.getItem().getId()));
+        Set<CartItem> cartItems = this.getCartItems();
+        cartItems.remove(cartItem);
+        cartItem.incrementQuantity();
+        cartItems.add(cartItem);
+        this.shoppingCartItems.setValue(cartItems);
+    }
+
+    public void decrementItemQuantity(final CartItem cartItem) {
+        this.userDataProvider.decrementShoppingCartItemQuantity(new SerializedCartItem(cartItem.getQuantity(),
+                cartItem.getColour(), cartItem.getSize(), cartItem.getItem().getId()));
+        Set<CartItem> cartItems = this.getCartItems();
+        cartItems.remove(cartItem);
+        cartItem.decrementQuantity();
+        cartItems.add(cartItem);
+        this.shoppingCartItems.setValue(cartItems);
+    }
+
+    public void changeItemQuantity(final CartItem cartItem, int newQuantity) {
+        this.userDataProvider.changeShoppingCartItemQuantity(new SerializedCartItem(cartItem.getQuantity(),
+                cartItem.getColour(), cartItem.getSize(), cartItem.getItem().getId()), newQuantity);
+        Set<CartItem> cartItems = this.getCartItems();
+        cartItems.remove(cartItem);
+        cartItem.changeQuantity(newQuantity);
+        cartItems.add(cartItem);
+        this.shoppingCartItems.setValue(cartItems);
+    }
+
+    private Set<CartItem> getCartItems() {
+        final Set<CartItem> items = this.shoppingCartItems.getValue();
+        if (items == null) return new HashSet<>();
+        return items;
     }
 }
