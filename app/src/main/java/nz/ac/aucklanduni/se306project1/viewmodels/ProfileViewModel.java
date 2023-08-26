f9b1e929681a4c7ac994e2ce492e8b06cf8d6f9c
KW781 <aarnobguha@gmail.com>
2023-08-26 16:22:43 +1200

    feat: :sparkles: created profile view model

---
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/ProfileViewModel.java
@@ -0,0 +1,28 @@
+package nz.ac.aucklanduni.se306project1.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.viewmodel.ViewModelInitializer;
+
+import nz.ac.aucklanduni.se306project1.EngiWearApplication;
+import nz.ac.aucklanduni.se306project1.dataproviders.AuthenticationProvider;
+
+public class ProfileViewModel extends ViewModel {
+    public static final ViewModelInitializer<ProfileViewModel> initializer = new ViewModelInitializer<>(
+            ProfileViewModel.class,
+            creationExtras -> {
+                final EngiWearApplication app = (EngiWearApplication) creationExtras.get(ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY);
+                assert app != null;
+                return new ProfileViewModel(app.getAuthenticationProvider());
+            });
+
+    private AuthenticationProvider authProvider;
+
+    public ProfileViewModel(AuthenticationProvider authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    public void signoutUser() {
+        this.authProvider.logoutUser();
+    }
+}
