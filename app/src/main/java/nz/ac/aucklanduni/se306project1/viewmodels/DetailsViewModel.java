e98b014c11d0c1e43e4c28809def26018c5550e8
KW781 <aarnobguha@gmail.com>
2023-08-26 23:11:18 +1200

    feat: :sparkles: added method to add item to cart in view model

--- app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/DetailsViewModel.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/viewmodels/DetailsViewModel.java
@@ -11,7 +11,10 @@ import java.util.List;
 
 import nz.ac.aucklanduni.se306project1.EngiWearApplication;
 import nz.ac.aucklanduni.se306project1.dataproviders.ItemDataProvider;
+import nz.ac.aucklanduni.se306project1.dataproviders.UserDataProvider;
 import nz.ac.aucklanduni.se306project1.models.items.ColouredItemInformation;
+import nz.ac.aucklanduni.se306project1.models.items.Item;
+import nz.ac.aucklanduni.se306project1.models.items.SerializedCartItem;
 
 public class DetailsViewModel extends ViewModel {
 
@@ -20,20 +23,23 @@ public class DetailsViewModel extends ViewModel {
             creationExtras -> {
                 final EngiWearApplication app = (EngiWearApplication) creationExtras.get(ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY);
                 assert app != null;
-                return new DetailsViewModel(app.getItemDataProvider());
+                return new DetailsViewModel(app.getItemDataProvider(), app.getUserDataProvider());
             });
     private final ItemDataProvider itemDataProvider;
+    private final UserDataProvider userDataProvider;
     private final MutableLiveData<ColouredItemInformation> selectedColourInfo = new MutableLiveData<>();
+    private Item item;
     private String selectedSize = null;
     private List<String> currentSizes = Collections.emptyList();
     private final MutableLiveData<Boolean> isInWatchlist = new MutableLiveData<>();
 
-    public DetailsViewModel(final ItemDataProvider itemDataProvider) {
+    public DetailsViewModel(final ItemDataProvider itemDataProvider, final UserDataProvider userDataProvider) {
         this.itemDataProvider = itemDataProvider;
+        this.userDataProvider = userDataProvider;
     }
 
-    public void setItemId(final String itemId) {
-        // TODO: Fetch if in watchlist
+    public void setItem(final String itemId) {
+        this.itemDataProvider.getItemById(itemId).thenAccept(fetchedItem -> this.item = fetchedItem);
     }
 
     public LiveData<Boolean> isInWatchlist() {
@@ -77,5 +83,10 @@ public class DetailsViewModel extends ViewModel {
 
     public ItemDataProvider getItemDataProvider() {
         return this.itemDataProvider;
+    }
+
+    public void addToCart() {
+        this.userDataProvider.addToShoppingCart(new SerializedCartItem(1, this.selectedColourInfo.getValue().getColour(),
+                this.selectedSize, this.item.getId()));
     }
 }
