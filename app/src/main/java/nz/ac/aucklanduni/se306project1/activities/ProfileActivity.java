bd20b239444e442195d9e1d30ff0fc114fa01ab0
PolarWolf314 <aaronjoshguo@gmail.com>
2023-08-26 15:21:03 +1200

    feat: ✨ can switch to profile page

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/ProfileActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/ProfileActivity.java
@@ -1,16 +1,28 @@
 package nz.ac.aucklanduni.se306project1.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+
 import nz.ac.aucklanduni.se306project1.R;
+import nz.ac.aucklanduni.se306project1.databinding.ActivityProfileBinding;
+import nz.ac.aucklanduni.se306project1.viewmodels.BottomNavigationViewModel;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private BottomNavigationViewModel bottomNavigationViewModel;
+    private ActivityProfileBinding binding;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        this.binding = ActivityProfileBinding.inflate(this.getLayoutInflater());
+        this.setContentView(this.binding.getRoot());
+        this.bottomNavigationViewModel = new ViewModelProvider(this).get(BottomNavigationViewModel.class);
+        this.bottomNavigationViewModel.setSelectedItemId(R.id.navigation_profile);
+        this.getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.red_top_bar));
+        this.getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.background_light_gray));
     }
 }
