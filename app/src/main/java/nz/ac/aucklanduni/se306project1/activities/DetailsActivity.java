7aaaaed945e50f96439ca70f683e452a630bf2dc
KW781 <aarnobguha@gmail.com>
2023-08-27 11:12:04 +1200

    fix: :bug: fixes crash due to adding item with single size

--- app/src/main/java/nz/ac/aucklanduni/se306project1/activities/DetailsActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/activities/DetailsActivity.java
@@ -146,10 +146,12 @@ public class DetailsActivity extends TopBarActivity {
         if (sizes.size() <= 1) {
             // Don't bother showing the sizes if there's only 1 option
             this.binding.sizeLabel.setVisibility(View.GONE);
+            this.detailsViewModel.setSelectedSize(sizes.get(0));
             return;
+        } else {
+            this.binding.sizeLabel.setVisibility(View.VISIBLE);
         }
 
-        this.binding.sizeLabel.setVisibility(View.VISIBLE);
 
         for (int index = 0; index < sizes.size(); index++) {
             final String size = sizes.get(index);
