056617c683604be5e0fb585a53f0e0e3cdd70aff
pumbas600 <joshjeffers600@gmail.com>
2023-08-27 16:58:59 +1200

    feat: :sparkles: Allow changing spinner colour

--- app/src/main/java/nz/ac/aucklanduni/se306project1/ui/LoadingSpinner.java
+++ app/src/main/java/nz/ac/aucklanduni/se306project1/ui/LoadingSpinner.java
@@ -1,14 +1,24 @@
 package nz.ac.aucklanduni.se306project1.ui;
 
+import android.content.res.ColorStateList;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import androidx.annotation.ColorInt;
 
 import nz.ac.aucklanduni.se306project1.R;
 
 public class LoadingSpinner {
-    private final View spinnerContainer;
+    private final ViewGroup spinnerContainer;
 
     public LoadingSpinner(final View root) {
         this.spinnerContainer = root.findViewById(R.id.spinner);
+    }
+
+    public void setColor(@ColorInt final int colourId) {
+        final ProgressBar spinner = (ProgressBar) this.spinnerContainer.getChildAt(0);
+        spinner.setIndeterminateTintList(ColorStateList.valueOf(colourId));
     }
 
     public void show() {
